af997f9b05d19649c30346056893a39383cfa81b
Ahmad <ahmedmageed19@gmail.com>
2020-03-22 14:28:42 +0200

    Auto stash before checking out "HEAD"

--- image.py
+++ image.py
@@ -1,35 +1,86 @@
 # This is the file which contains the image class
 import cv2 as cv
-
+import numpy as np
 
 class image():
-    """
-    this class is the maintaining class for all actions done on an image
-    """
     def __init__(self):
         self.imageData = None
         self.imageFourier = None
+        self.imageFourierInv = None
+        self.dataType = None
 
-
-
-    def loadImage(self, path):
+    def loadImage(self, path: str):
         """
-        loads the specified image from its path
-        :return: image data array
+        loads the specified image from its path and normalize it
+        :return: image data array of a gray scale image
         """
         self.imageData = cv.imread(path)
+        self.imageData = cv.cvtColor(self.imageData, cv.COLOR_RGB2GRAY)/255.0
+        self.dataType = self.imageData.dtype
 
     def fourierTransform(self):
         self.imageFourier = cv.dft(self.imageData)
 
+    def inverseFourier(self):
+        self.imageFourierInv = cv.idft(self.imageFourier)
+
+    def realComponent(self):
+        return np.real(self.imageFourier)
+
+    def imaginaryComponent(self):
+        return np.imag(self.imageFourier)
+
+    def magnitude(self):
+            return np.abs(self.imageFourier)
+
+    def phase(self):
+        return np.angle(self.imageFourier)
+
+
+class mixer2Image():
+    def __init__(self):
+        self.imagesTransformed = []
+        self.realComponents = []
+        self.imaginaryComponents = []
+        self.imagesPhase = []
+        self.imagesMagnitude = []
+
+    def addImage(self, image):
+        self.imagesTransformed.append(image)
+
+        self.imagesMagnitude.append(image.magnitude())
+        self.imagesPhase.append(image.phase())
+        self.realComponents.append(image.realComponent())
+        self.imaginaryComponents.append(image.imaginaryComponent())
+
+    def mixRealImg(self, R: float, I:float, img: int) -> complex:
+        real = R * self.realComponents[img] + (1-R)*self.realComponents[~img]
+        print(real)
+        imaginary = I * self.imaginaryComponents[img] + (1-I)*self.imaginaryComponents[~img]
+        print(imaginary)
+        return np.complex(real, imaginary)
+
+    def mixPhaseMagnitude(self):
+        pass
+
+
 
 if __name__ == '__main__':
-    img = image()
-    img.loadImage("TestImages/cat-1792684_640.jpg")
-    print(img.imageData)
-    img.fourierTransform()
-    print(img.imageFourier)
+    img1 = image()
+    img1.loadImage("TestImages/tree-736877_640.jpg")
+    img1.fourierTransform()
 
+    img2 = image()
+    img2.loadImage("TestImages/landscape-4938188_640.jpg")
+    img2.fourierTransform()
+
+    mixer = mixer2Image()
+
+    mixer.addImage(img1)
+    mixer.addImage(img2)
+
+    output1 = mixer.mixRealImg(0.6,0.8, 0)
+    print(output1)
 
 
 
--- main.py
+++ main.py
@@ -1,20 +1,59 @@
 # Main Application Program
 import mainUI as ui
 from PyQt5 import QtCore, QtGui, QtWidgets
+import image
+import cv2
+import pyqtgraph as pg
 
 
 class phaseMonster(ui.Ui_MainWindow):
     # Main Application Class
-    pass
+    def __init__(self, starterWindow):
+        super(phaseMonster, self).setupUi(starterWindow)
+        self.mixer = None # holds the mixer class from image.py
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # # pg.setConfigOption('imageAxisOrder', 'row-major')
+        # self.img = cv2.imread("TestImages/lelouch_lamperouge_code_geass_zero_102069_1920x1080.jpg")[:, :, 0]
+        # print(self.img.ndim)
+        # print(self.img.T.shape)
+        # print(self.img.dtype)
+        #
+        # self.imageTwoOrigin.setImage(self.img.T, autoHistogramRange=True)
+        #
+        # self.imageTwoOrigin.view.setAspectLocked(False)
+        # self.imageTwoOrigin.view.setRange(xRange=[0, self.img.T.shape[0]], yRange=[0, self.img.T.shape[1]], padding=0)
+        # self.imageTwoOrigin.ui.histogram.hide()
+        # self.imageTwoOrigin.ui.roiBtn.hide()
+        # self.imageTwoOrigin.ui.menuBtn.hide()
+        # self.imageTwoOrigin.ui.roiPlot.hide()
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = phaseMonster()
-    ui.setupUi(MainWindow)
+    ui = phaseMonster(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
+    #
+    # import pyqtgraph.examples
+    #
+    # pyqtgraph.examples.run()
 
